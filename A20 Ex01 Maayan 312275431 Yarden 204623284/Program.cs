94c04b1b111ed05864dba04784193d6a646aadd6
Yarden <yardenrachamim1993@gmail.com>
2019-11-29 16:13:58 +0200

    Add project files.

---
+++ A20 Ex01 Maayan 312275431 Yarden 204623284/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace A20_Ex01_Maayan_312275431_Yarden_204623284
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
